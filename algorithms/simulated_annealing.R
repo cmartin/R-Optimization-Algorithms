1ebfda9dd7257f375446c4deb3b34ce1a3a5b236
Charles Martin <martin.charles@gmail.com>
2016-05-19 13:30:39 -0400

    Added comments

--- algorithms/simulated_annealing.R
+++ algorithms/simulated_annealing.R
@@ -15,25 +15,35 @@ simulated_annealing_minimizer <- function(
 
 ){
 
-  theta <- runif(n_params)
+  theta <- runif(n_params) # start with random values
 
-  current_y <- -300000
+  current_y <- -300000 # arbitrary small "maximum" value
 
   y_trace <- c()
 
   for (i in 1:max_iterations) {
+
+    # for each parameter
     for (j in 1:n_params) {
       theta1 <- theta
+
+      # pick another point around the original ont
       theta1[j] <- theta1[j] + runif(n = 1, min = -max_search_distance, max = max_search_distance)
 
-      new_y <- -f(theta1) # turn algorithm into a minimizer
+      # calculate function value for new params
+      new_y <- -f(theta1) # minus signs turn algorithm into a minimizer
 
+      # calculate how much better (or worse) are the new params
       delta_h <- new_y - current_y
 
-      #temp <- temp_for_progress(i / max_iterations, start_temp)
+      # calculate temperature based on iteration number
       temp <- start_temp * temperature_curve ^ i
+
+      # calculate probability of acceptance of params worse than originals
       p <- exp(delta_h / temp)
       if ( (delta_h > 0) || (runif(1) < p)) {
+
+        # accept new param values
         theta <- theta1
         current_y <- new_y
       }
@@ -42,6 +52,7 @@ simulated_annealing_minimizer <- function(
 
     y_trace[length(y_trace) + 1] <- current_y
 
+    # Calculate stopping condition only on max_stall_iterations values
     y_to_test <- y_trace[
       (max(length(y_trace) - max_stall_iterations,1)
       ):length(y_trace)
