5efa8c63130232e711778757286479844d874637
Charles Martin <martin.charles@gmail.com>
2016-05-19 10:38:10 -0400

    Most of the time, now stopping with a solution

--- algorithms/simulated_annealing.R
+++ algorithms/simulated_annealing.R
@@ -1,4 +1,7 @@
 rm(list = ls())
+
+source("utils/se.R")
+
 delta <- function() {
   runif(n = 1, min = -.5, max = +.5)
 }
@@ -16,14 +19,19 @@ temp_for_progress <- function(progress, start_temp) { # Progress is 0 at first, 
 simulated_annealing <- function(
   n_params = 1,
   f,
-  max_iterations = 50,
+  max_iterations = 100000,
   start_temp = 3,
-  verbose = TRUE
+  verbose = TRUE,
+  tolerance = 1e-5,
+  max_stall_iterations = 500 * n_params
+
 ){
 
   theta <- runif(n_params)
 
   current_y <- -300000
+
+  y_trace <- c()
 
   for (i in 1:max_iterations) {
     for (j in 1:n_params) {
@@ -42,8 +50,25 @@ simulated_annealing <- function(
 
     }
 
+    y_trace[length(y_trace) + 1] <- current_y
+
+    y_to_test <- y_trace[
+      (max(length(y_trace) - max_stall_iterations,1)
+      ):length(y_trace)
+    ]
+    se <- std_err(y_to_test)
+
+    if (!is.na(se) && (se < tolerance)){
+      cat(paste0("Solution reached at iter ",i,"\r\n"))
+      break
+    }
+
     if (verbose) { print(paste(i,current_y, theta, temp)) }
 
+  }
+
+  if (i == max_iterations) {
+    warning(paste("Algorithm did not converge in", i, "iterations\r\n"))
   }
 
   return(theta)
@@ -52,4 +77,4 @@ simulated_annealing <- function(
 
 
 source("test_functions/MathewsFink.R")
-simulated_annealing(2,mf_f, max_iterations = 100)
+simulated_annealing(2,mf_f, max_iterations = 100000, verbose = FALSE)
